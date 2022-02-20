c37cfc4bab027700605346c2d30c650e5baa17e4
bst003 <bstanner33@yahoo.com>
2022-02-20 17:16:52 -0500

    Create operate function

--- scripts.js
+++ scripts.js
@@ -22,3 +22,31 @@ function multiply( a, b) {
 function divide( a, b) {
     return a / b;
 }
+
+function operate( val1, val2, operator) {
+
+    let result;
+
+    switch(operator){
+
+        case '+':
+            result = add(val1, val2);
+            break;
+
+        case '-':
+            result = subtract(val1, val2);
+            break;
+        
+        case '*':
+            result = multiply(val1, val2);
+            break;
+
+        case '/':
+            result = divide(val1, val2);
+            break;  
+
+    }
+
+    return result;
+
+}
